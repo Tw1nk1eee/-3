f73d9d8151283f6569e84835a62a2233a8a089f8
SergeyBarabanshikov <kovalyov-2001@list.ru>
2020-05-14 14:45:01 +0500

    в программу добавлена строчка для нахождения произведения 2-х чисел

--- summa.pas
+++ summa.pas
@@ -1,9 +1,11 @@
 ﻿program qq;
 uses crt;
-var a,b,summ:real;
+var a,b,summ,proizv:real;
 begin
   readln(a);
   readln(b);
   summ:=a+b;
+  proizv:=a*b;
+  writeln('Произведение двух чисел = ',proizv);
   writeln('Сумма чисел равна =',summ);
 end.
