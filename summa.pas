97881a4736f8375859e79f52b380f0b7d3bc21f6
SergeyBarabanshikov <kovalyov-2001@list.ru>
2020-05-14 14:47:06 +0500

    в программу добавлена строчка нахождения разности произведения и суммы 2-х чисел

--- summa.pas
+++ summa.pas
@@ -1,11 +1,13 @@
 ﻿program qq;
 uses crt;
-var a,b,summ,proizv:real;
+var a,b,summ,proizv,razn:real;
 begin
   readln(a);
   readln(b);
   summ:=a+b;
   proizv:=a*b;
-  writeln('Произведение двух чисел = ',proizv);
-  writeln('Сумма чисел равна =',summ);
+  razn:=(a*b)-(a+b);
+  writeln('Разность чисел равна  =',razn);
+  writeln('Произведение двух чисел  =',proizv);
+  writeln('Сумма чисел равна  =',summ);
 end.
